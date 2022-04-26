261bf879b1719e99b4cc82ee06ed042e96195588
penglu <lt950330>
2022-04-26 21:03:54 +0800

    530. 二叉搜索树的最小绝对差

--- src/main/java/算法/二叉树/TreeCode.java
+++ src/main/java/算法/二叉树/TreeCode.java
@@ -481,6 +481,43 @@ public class TreeCode {
         return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
     }
 
+
+    /**
+     * 530. 二叉搜索树的最小绝对差
+     * https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+     * 给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。
+     * 差值是一个正数，其数值等于两值之差的绝对值。
+     *
+     * 输入：root = [4,2,6,1,3]
+     * 输出：1
+     *
+     * 遇到在二叉搜索树上求什么最值，求差值之类的，都要思考一下二叉搜索树可是有序的，要利用好这一特点。
+     *
+     */
+    public int getMinimumDifference(TreeNode root) {
+        int min = Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList<>();
+        midRoot(root, list);
+        int j = 1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            min = Math.min(min, list.get(j) - list.get(i));
+            j++;
+        }
+        return min;
+    }
+
+    /**
+     * 先中序遍历得到一个有序数组
+     */
+    private void midRoot(TreeNode node, List<Integer> num) {
+        if (node == null) {
+            return;
+        }
+        midRoot(node.left, num);
+        num.add(node.val);
+        midRoot(node.right, num);
+    }
+
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5, 0};
         System.out.println(a[0]);
