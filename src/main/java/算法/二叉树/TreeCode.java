51e64b1d293c33114244b5a9961a64a3d4632ace
penglu <lt950330>
2022-05-14 15:04:26 +0800

    669. 修剪二叉搜索树

--- src/main/java/算法/二叉树/TreeCode.java
+++ src/main/java/算法/二叉树/TreeCode.java
@@ -657,13 +657,41 @@ public class TreeCode {
             if (root.right == null) {
                 return root.left;
             }
-            TreeNode node = root.right;
+            TreeNode node = root.right; // 这里只是对象的引用,并未修改root对象本身
             while (node.left != null) {
                 node = node.left;
             }
             node.left = root.left;
             return root.right;
         }
+    }
+
+    /**
+     * 669. 修剪二叉搜索树
+     * https://leetcode.cn/problems/trim-a-binary-search-tree/
+     * 给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。修剪树 不应该 改变保留在树中的元素的相对结构 (即，如果没有被移除，原有的父代子代关系都应当保留)。 可以证明，存在 唯一的答案 。
+     *
+     * 所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。
+     *
+     * 输入：root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+     * 输出：[3,2,null,1]
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+        if (root.val < low) {
+            //如果节点数小于最小值, 则裁掉所有左节点.
+            root = root.right;
+            //裁掉后继续检查是否符合
+            root = trimBST(root, low, high);
+        } else if (root.val > high) {
+            //如果节点数大于最大值,则裁掉所有右节点
+            root = root.left;
+            root = trimBST(root, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        }
+        return root;
     }
 
     public static void main(String[] args) {
