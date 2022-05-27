e69f1eb0786ae1014cd7107f186610711fbbb987
penglu <lt950330>
2022-05-27 20:36:12 +0800

    78. 子集

--- src/main/java/算法/backtracking/BackTracking.java
+++ src/main/java/算法/backtracking/BackTracking.java
@@ -341,8 +341,36 @@ public class BackTracking {
     }
 
 
+    /**
+     * 78. 子集
+     * https://leetcode.cn/problems/subsets/
+     * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+     * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+     *
+     * 示例 1：
+     * 输入：nums = [1,2,3]
+     * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+     *
+     */
+    List<List<Integer>> result7 = new ArrayList<>();
+    LinkedList<Integer> path7 = new LinkedList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        result7.add(new ArrayList<>(null));
+        subsets(nums, 0);
+        return result7;
+    }
 
-
+    private void subsets(int[] nums, int startIndex) {
+        result7.add(new ArrayList<>(path7));//「遍历这个树的时候，把所有节点都记录下来，就是要求的子集集合」。
+        if (startIndex >= nums.length){ //终止条件可不加
+            return;
+        }
+        for (int i = startIndex; i < nums.length; i++) {
+            path7.add(nums[i]);
+            subsets(nums, i + 1);
+            path7.removeLast();
+        }
+    }
 
     public static void main(String[] args) {
         restoreIpAddresses("25525511135");
