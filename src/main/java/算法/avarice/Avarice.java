1902662ad70a0e7949f8b55b1d8cfc13dd6bfcc5
penglu <lt950330>
2022-05-30 20:43:06 +0800

    376. 摆动序列

--- src/main/java/算法/avarice/Avarice.java
+++ src/main/java/算法/avarice/Avarice.java
@@ -1,6 +1,7 @@
 package 算法.avarice;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
 
 /**
  * @description: 贪心算法
@@ -46,6 +47,65 @@ public class Avarice {
             sIdx ++;
         }
         return child;
+    }
+
+
+    /**
+     * 376. 摆动序列
+     * https://leetcode.cn/problems/wiggle-subsequence/
+     * 如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素的序列也视作摆动序列。
+     *
+     * 例如，[1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3)是正负交替出现的。
+     * 相反，[1, 4, 7, 2, 5]和[1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+     * 子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序。
+     * 给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度 。
+     *
+     * 示例 1：
+     * 输入：nums = [1,7,4,9,2,5]
+     * 输出：6
+     * 解释：整个序列均为摆动序列，各元素之间的差值为 (6, -3, 5, -7, 3) 。
+     */
+    public int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums.length;
+        }
+        //当前差值
+        int curDiff = 0;
+        //上一个差值
+        int preDiff = 0;
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            //得到当前差值
+            curDiff = nums[i] - nums[i - 1];
+            //如果当前差值和上一个差值为一正一负
+            //等于0的情况表示初始时的preDiff
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                count++;
+                preDiff = curDiff;
+            }
+        }
+        return count;
+        /**
+         * int n = nums.length;
+         *         if (n < 2) {
+         *             return n;
+         *         }
+         *         int up = 1;
+         *         int down = 1;
+         *         for (int i = 1; i < n; i++) {
+         *             if (nums[i] > nums[i - 1]) {
+         *                 up = down + 1;
+         *             }
+         *             if (nums[i] < nums[i - 1]) {
+         *                 down = up + 1;
+         *             }
+         *         }
+         *         return Math.max(up, down);
+         */
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,7,4,9,2,5};
     }
 
 
