053db7415d5a0e91f03b97098c0375d200da3a45
penglu <lt950330>
2022-06-01 20:24:57 +0800

    122. 买卖股票的最佳时机 II

--- src/main/java/算法/avarice/Avarice.java
+++ src/main/java/算法/avarice/Avarice.java
@@ -1,5 +1,9 @@
 package 算法.avarice;
 
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 
@@ -127,6 +131,32 @@ public class Avarice {
                 sum = num;
             }
             res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+    /**
+     * 122. 买卖股票的最佳时机 II
+     * 给你一个整数数组 prices ，其中prices[i] 表示某支股票第 i 天的价格。
+     * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候最多只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+     * 返回 你能获得的 最大 利润。
+     * 
+     * 示例 1：
+     * 输入：prices = [7,1,5,3,6,4]
+     * 输出：7
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+     *     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
+     *      总利润为 4 + 3 = 7 。
+     *
+     *
+     */
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            //只要今天的价格比昨天高,就卖出股票,得出利润
+            if (prices[i] > prices[i - 1]) {
+                res += prices[i] - prices[i-1];
+            }
         }
         return res;
     }
