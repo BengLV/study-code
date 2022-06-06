06b292915aa5709908e4a1e0f825f9ff2a356802
penglu <lt950330>
2022-06-06 21:14:00 +0800

    45. 跳跃游戏 II

--- src/main/java/算法/avarice/Avarice.java
+++ src/main/java/算法/avarice/Avarice.java
@@ -185,6 +185,46 @@ public class Avarice {
         return false;
     }
 
+
+    /**
+     * 45. 跳跃游戏 II
+     * 给你一个非负整数数组nums ，你最初位于数组的第一个位置。
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+     * 假设你总是可以到达数组的最后一个位置。
+     *
+     * 示例 1:
+     *
+     * 输入: nums = [2,3,1,1,4]
+     * 输出: 2
+     * 解释: 跳到最后一个位置的最小跳跃数是 2。
+     *     从下标为 0 跳到下标为 1 的位置，跳1步，然后跳3步到达数组的最后一个位置。
+     *
+     */
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) {
+            return 0;
+        }
+        int ans = 0;//跳跃次数
+        int curRange = 0;//当前下标值能覆盖的最大范围
+        int maxRange = 0;//覆盖的最大范围
+        for (int i = 0; i <= nums.length; i++) {
+            //获取当前下表最大覆盖范围
+            maxRange = Math.max(maxRange, i + nums[i]);
+            //超出数组长度,则返回
+            if (maxRange >= nums.length - 1) {
+                ans++;
+                break;
+            }
+            //如果当前下标值等于当前能覆盖的最大范围,则更新最大范围,以及次数加一
+            if (i == curRange) {
+                curRange = maxRange;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,7,4,9,2,5};
     }
