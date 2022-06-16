1c6c504dc2f047414fd8b8e89cabf624740baca1
penglu <lt950330>
2022-06-16 19:47:33 +0800

    62. 不同路径

--- src/main/java/算法/dynamicprogram/DynamicProgramming.java
+++ src/main/java/算法/dynamicprogram/DynamicProgramming.java
@@ -106,4 +106,33 @@ public class DynamicProgramming {
         return Math.min(dp[cost.length], dp[cost.length - 1]);
     }
 
+    /**
+     * 62. 不同路径
+     * https://leetcode.cn/problems/unique-paths/
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+     * 问总共有多少条不同的路径？
+     *
+     * 1. 确定dp数组下表含义 dp[i][j] 到每一个坐标可能的路径种类
+     * 2. 递推公式 dp[i][j] = dp[i-1][j] dp[i][j-1]
+     * 3. 初始化 dp[i][0]=1 dp[0][i]=1 初始化横竖就可
+     * 4. 遍历顺序 一行一行遍历
+     * 5. 推导结果 。。。。。。。。
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i ++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i ++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i ++) {
+            for (int j = 1; j < n; j ++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
 }
