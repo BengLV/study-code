88dd41de078d36c69d048493d9fb7d4bcc9fb6df
penglu <lt950330>
2022-06-21 20:10:00 +0800

    343. 整数拆分

--- src/main/java/算法/dynamicprogram/DynamicProgramming.java
+++ src/main/java/算法/dynamicprogram/DynamicProgramming.java
@@ -176,4 +176,29 @@ public class DynamicProgramming {
         return dp[m - 1][n - 1];
     }
 
+
+    /**
+     * 343. 整数拆分
+     * https://leetcode.cn/problems/integer-break/
+     * 给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+     * 返回 你可以获得的最大乘积
+     *
+     * 示例 1:
+     * 输入: n = 10
+     * 输出: 36
+     * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+     */
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i ++) {
+            for (int j = 1; j < i; j ++) {
+                //j*(i-j)代表把i拆分为j和i-j两个数相乘
+                //j*dp[i-j]代表把i拆分成j和继续把(i-j)这个数拆分，取(i-j)拆分结果中的最大乘积与j相乘
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+
 }
