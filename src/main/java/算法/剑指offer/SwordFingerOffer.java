1f0e265e553ee6b7ff24a99bf58494560bb2b973
penglu <lt950330>
2022-06-30 19:28:27 +0800

    剑指 Offer 05. 替换空格

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -130,6 +130,32 @@ public class SwordFingerOffer {
         return false;
     }
 
+
+    /**
+     * 剑指 Offer 05. 替换空格
+     * https://leetcode.cn/problems/ti-huan-kong-ge-lcof/
+     *
+     * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+     *
+     * 示例 1：
+     * 输入：s = "We are happy."
+     * 输出："We%20are%20happy."
+     *
+     */
+    public String replaceSpace(String s) {
+        //return s.replaceAll(" ", "%20");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                builder.append("%20");
+            } else {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);
