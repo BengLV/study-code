8c33c815ae8ebf8bc5457169378db32bc5f26854
penglu <lt950330>
2022-07-15 19:38:50 +0800

    剑指 Offer 13. 机器人的运动范围

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -381,6 +381,36 @@ public class SwordFingerOffer {
         return ans;
     }
 
+
+    /**
+     * 剑指 Offer 13. 机器人的运动范围
+     * https://leetcode.cn/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/
+     *
+     * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，
+     * 它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。
+     * 例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+     *
+     * 示例 1：
+     * 输入：m = 2, n = 3, k = 1
+     * 输出：3
+     */
+    public int movingCount(int m, int n, int k) {
+        //判断是否走过走过某个节点
+        boolean[][] visited = new boolean[m][n];
+        return dfs(m, n, k, 0, 0, visited);
+    }
+
+    private int dfs(int m, int n, int k, int i, int j, boolean[][] visited) {
+        //终止条件,返回0
+        if (i >= m || i < 0 || j >= n || j < 0 || (i / 10 + i % 10 + j / 10 + j % 10) > k || visited[i][j]) {
+            return 0;
+        }
+        //标记节点走过
+        visited[i][j] = true;
+        return 1 + dfs(m, n, k, i - 1, j, visited) + dfs(m, n, k, i + 1, j, visited)
+                + dfs(m, n, k, i, j - 1, visited) + dfs(m, n, k, i, j + 1, visited);
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);
