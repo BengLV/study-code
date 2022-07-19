713c00724048b0950c59758ac62bc4c24cdf4a6a
penglu <lt950330>
2022-07-19 21:02:57 +0800

    剑指 Offer 14- II. 剪绳子 II

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -446,6 +446,35 @@ public class SwordFingerOffer {
         return dp[n];
     }
 
+
+    /**
+     * 剑指 Offer 14- II. 剪绳子 II
+     * https://leetcode.cn/problems/jian-sheng-zi-ii-lcof/
+     *
+     * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m - 1] 。
+     * 请问 k[0]*k[1]*...*k[m - 1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+     *
+     * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     *
+     *
+     */
+    public int cuttingRope2(int n) {
+        //首先排除特殊情况
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        long res = 1;
+        while (n > 4) {
+            //包含3最多,结果最优
+            res *= 3;
+            //先对结果取模.
+            res = res % 1000000007;
+            n -= 3;
+        }
+        //可能剩余1,2,3. 乘以res即可.
+        return (int)(res * n % 1000000007);
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);
