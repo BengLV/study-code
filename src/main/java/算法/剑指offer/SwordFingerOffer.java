ba15bd7b36d49325dc74304d1bb2ab94e3ad7e1d
penglu <lt950330>
2022-07-21 19:44:03 +0800

    剑指 Offer 15. 二进制中1的个数

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -475,11 +475,44 @@ public class SwordFingerOffer {
         return (int)(res * n % 1000000007);
     }
 
+
+    /**
+     * 剑指 Offer 15. 二进制中1的个数
+     * https://leetcode.cn/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/
+     *
+     * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为 汉明重量).）。
+     *
+     * 示例 1：
+     *
+     * 输入：n = 11 (控制台输入 00000000000000000000000000001011)
+     * 输出：3
+     * 解释：输入的二进制串 00000000000000000000000000001011中，共有三位为 '1'。
+     *
+     * 位运算
+     *
+     */
+    public static int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            res++;
+            /**
+             * A = 0011 1100
+             * B = 0000 1101
+             * ＆如果相对应位都是1，则结果为1，否则为0
+             * （A＆B），得到12，即0000 1100
+             */
+            //消去数字n转为二进制后最右边的 1
+            n &= n - 1;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);
         System.out.println(a.length);
         System.out.println(a[1][2]);
+        System.out.println(hammingWeight(13));
     }
 
 
