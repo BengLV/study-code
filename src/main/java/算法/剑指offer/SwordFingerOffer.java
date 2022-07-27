e482e856ce4eaae7ac54d6ec25cb98ad7570677e
penglu <lt950330>
2022-07-27 20:20:53 +0800

    剑指 Offer 20. 表示数值的字符串

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -739,7 +739,75 @@ public class SwordFingerOffer {
     }
 
 
-
+    /**
+     * 剑指 Offer 20. 表示数值的字符串
+     * https://leetcode.cn/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof/
+     *
+     * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+     *
+     * 数值（按顺序）可以分成以下几个部分：
+     *
+     * 若干空格
+     * 一个小数或者整数
+     * （可选）一个'e'或'E'，后面跟着一个整数
+     * 若干空格
+     * 小数（按顺序）可以分成以下几个部分：
+     *
+     * （可选）一个符号字符（'+' 或 '-'）
+     * 下述格式之一：
+     * 至少一位数字，后面跟着一个点 '.'
+     * 至少一位数字，后面跟着一个点 '.' ，后面再跟着至少一位数字
+     * 一个点 '.' ，后面跟着至少一位数字
+     * 整数（按顺序）可以分成以下几个部分：
+     *
+     * （可选）一个符号字符（'+' 或 '-'）
+     * 至少一位数字
+     * 部分数值列举如下：
+     *
+     * ["+100", "5e2", "-123", "3.1416", "-1E-16", "0123"]
+     * 部分非数值列举如下：
+     *
+     * ["12e", "1a3.14", "1.2.3", "+-5", "12e+5.4"]
+     *
+     * 示例 1：
+     * 输入：s = "0"
+     * 输出：true
+     * 
+     * 示例 2：
+     * 输入：s = "."
+     * 输出：false
+     * 
+     * 示例 3：
+     * 输入：s = "    .1  "
+     * 输出：true
+     */
+    public boolean isNumber(String s) {
+        if(s == null || s.length() == 0) return false;
+        s = s.trim();//去除空格
+        boolean numFlag = false;//是否为数字,返回结果
+        boolean dotFlag = false;//是否出现.
+        boolean eFlag = false;//是否出现e
+        for (int i = 0; i < s.length(); i++) {
+            //是否为数字
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                numFlag = true;
+                //是否为.,需要之前没出现过. 并且没出现e
+            } else if (s.charAt(i) == '.' && !dotFlag && !eFlag) {
+                dotFlag = true;
+                //是否为e,需要之前没出现过e,并且出现过数字了
+            } else if ((s.charAt(i) == 'e' || s.charAt(i) == 'E') && !eFlag && numFlag) {
+                eFlag = true;
+                numFlag = false;//防止出现123e这种情况,出现e就标记为false.
+                //是否为+ -符号,只能出现在第一位或e后面
+            } else if ((s.charAt(i) == '+' || s.charAt(i) == '-') && (i == 0 || s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E')) {
+                //不做任何操作.
+            } else {
+                //其他情况都是非法的
+                return false;
+            }
+        }
+        return numFlag;
+    }
 
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
