6d827546d4690b7af03bebe65e75321b5fa62533
penglu <lt950330>
2022-07-29 19:34:55 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -809,6 +809,65 @@ public class SwordFingerOffer {
         return numFlag;
     }
 
+
+    /**
+     * 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+     * https://leetcode.cn/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+     *
+     * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数在数组的前半部分，所有偶数在数组的后半部分。
+     *
+     * 示例：
+     * 输入：nums = [1,2,3,4]
+     * 输出：[1,3,2,4]
+     * 注：[3,1,2,4] 也是正确的答案之一。
+     */
+    public static int[] exchange(int[] nums) {
+        /*额外数组
+        int[] res = new int[nums.length];
+        int pre = 0;//记录偶数出现的次数
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & 1) == 1) {//是否为奇数
+                res[i - pre] = nums[i];
+            } else {
+                res[nums.length - pre - 1] = nums[i];
+                pre++;
+            }
+
+        }
+        return res;
+        */
+        /*首尾指针
+        int right = nums.length - 1;
+        int left = 0;
+        while (left < right) {
+            if ((nums[left] & 1) == 1) {//为奇数
+                left++;
+                continue;
+            }
+            if ((nums[right] & 1) == 0) {//为偶数
+                right--;
+                continue;
+            }
+            //交换
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+        return nums;*/
+        int fast = 0;//快指针
+        int low = 0;//慢指针
+        while (fast < nums.length) {
+            if ((nums[fast] & 1) == 1) {
+                int temp = nums[low];
+                nums[low] = nums[fast];
+                nums[fast] = temp;
+                low++;
+            }
+            fast++;
+        }
+        return nums;
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);//3
