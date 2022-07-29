cc35d3819abea8560bb78eccc619276b29712572
penglu <lt950330>
2022-07-29 20:29:51 +0800

    剑指 Offer 22. 链表中倒数第k个节点

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -868,6 +868,47 @@ public class SwordFingerOffer {
         return nums;
     }
 
+    /**
+     * 剑指 Offer 22. 链表中倒数第k个节点
+     * https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+     *
+     * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+     * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+     *
+     * 示例：
+     * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+     * 返回链表 4->5.
+     *
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+       /* ListNode temp = head;
+        int count = 0;
+        while (temp != null) {
+            //先遍历获取count
+            count++;
+            temp = temp.next;
+        }
+        temp = head;
+        int j = 1;
+        while (temp != null) {
+            if (j == count - k + 1) {
+                return temp;
+            }
+            temp = temp.next;
+            j++;
+        }
+        return null;*/
+        ListNode fast = head, low = head;//快慢指针
+        for (int i = 0; i < k; i++) {//快指针先走k步试试
+            fast = fast.next;
+        }
+        while (fast != null) {//快指针比慢指针多走k步.快指针走完时,慢指针还剩k步没有走.返回即可.
+            fast = fast.next;
+            low = low.next;
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);//3
