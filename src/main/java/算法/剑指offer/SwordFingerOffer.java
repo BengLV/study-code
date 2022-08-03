83882b9f4c7b068c0546ff74196ea50c035a3dbb
penglu <lt950330>
2022-08-03 19:32:21 +0800

    剑指 Offer 29. 顺时针打印矩阵

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -1112,6 +1112,51 @@ public class SwordFingerOffer {
     }
 
 
+    /**
+     * 剑指 Offer 29. 顺时针打印矩阵
+     * https://leetcode.cn/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+     *
+     * 示例 1：
+     * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * 输出：[1,2,3,6,9,8,7,4,5]
+     */
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix == null) return null;
+        int row = matrix.length;
+        if (row == 0) return new int[0];
+        int col = matrix[0].length;
+        int[] res = new int[row * col];
+        int idx = 0;
+        int left = 0, top = 0, right = col - 1, bottom = row - 1;//边界值, 左上右下
+        while (true) {
+            //从左往右走
+            for (int i = left; i <= right; i++) {
+                res[idx++] = matrix[top][i];
+            }
+            top ++;
+            if (top > bottom) break;
+            //从上往下走
+            for (int i = top; i <= bottom; i++) {
+                res[idx++] = matrix[i][right];
+            }
+            right--;
+            if (right < left) break;
+            //从右往左走
+            for (int i = right; i >= left; i--) {
+                res[idx++] = matrix[bottom][i];
+            }
+            bottom--;
+            if (bottom < top) break;
+            //从下往上走
+            for (int i = bottom; i >= top; i--) {
+                res[idx++] = matrix[i][left];
+            }
+            left++;
+            if (left > right) break;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);//3
