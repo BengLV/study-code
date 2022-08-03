21130ae3288d09a9411517ea7d9ff4f670909bc5
penglu <lt950330>
2022-08-03 18:45:04 +0800

    剑指 Offer 26. 树的子结构

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -978,6 +978,46 @@ public class SwordFingerOffer {
         }
     }
 
+
+    /**
+     * 剑指 Offer 26. 树的子结构
+     * https://leetcode.cn/problems/shu-de-zi-jie-gou-lcof/
+     *
+     * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+     * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+     * 
+     * 例如:
+     * 给定的树 A:
+     *   3
+     *   / \
+     *  4  5
+     *  / \
+     * 1  2
+     *
+     * 给定的树 B：
+     *
+     *  4
+     *  /
+     * 1
+     * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+     *
+     * 示例 1：
+     * 输入：A = [1,2,3], B = [3,1]
+     * 输出：false
+     *
+     */
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) return false;
+        return dfs(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    public boolean dfs(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null) return false;
+        return A.val == B.val && dfs(A.left, B.left) && dfs(A.right, B.right);
+    }
+
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);//3
