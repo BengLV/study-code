6cdb1eeb084dbc4d2c072b1f4f230cd070fc10d8
penglu <lt950330>
2022-08-03 18:43:06 +0800

    剑指 Offer 25. 合并两个排序的链表

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -934,6 +934,50 @@ public class SwordFingerOffer {
         return pre;
     }
 
+
+    /**
+     * 剑指 Offer 25. 合并两个排序的链表
+     * https://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
+     *
+     * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+     *
+     * 示例1：
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     *
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        /**
+         * ListNode res = new ListNode(-1);
+         *         ListNode temp = res;
+         *         while (l1 != null && l2 != null) {
+         *             if (l1.val <= l2.val) {
+         *                 temp.next = l1;
+         *                 l1 = l1.next;
+         *             } else {
+         *                 temp.next = l2;
+         *                 l2 = l2.next;
+         *             }
+         *             temp = temp.next;
+         *         }
+         *         temp.next = l1 != null ? l1 : l2;
+         *         return res.next;
+         */
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.data <= l2.data) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args) {
         int a[][]={{1,2,3},{4,5,6}};
         System.out.println(a[0].length);//3
