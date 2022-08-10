8a805406bec14501648d5fc3876df920a7939f97
penglu <lt950330>
2022-08-10 20:22:03 +0800

    剑指 Offer 32 - I. 从上到下打印二叉树

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -1100,6 +1100,26 @@ public class SwordFingerOffer {
         return true;
     }
 
+    /**
+     * 做递归思考三步：
+     *
+     * 递归的函数要干什么？
+     * 函数的作用是判断传入的两个树是否镜像。
+     * 输入：TreeNode left, TreeNode right
+     * 输出：是：true，不是：false
+     *
+     * 递归停止的条件是什么？
+     * 左节点和右节点都为空 -> 倒底了都长得一样 ->true
+     * 左节点为空的时候右节点不为空，或反之 -> 长得不一样-> false
+     * 左右节点值不相等 -> 长得不一样 -> false
+     *
+     * 从某层到下一层的关系是什么？
+     * 要想两棵树镜像，那么一棵树左边的左边要和二棵树右边的右边镜像，一棵树左边的右边要和二棵树右边的左边镜像
+     * 调用递归函数传入左左和右右
+     * 调用递归函数传入左右和右左
+     * 只有左左和右右镜像且左右和右左镜像的时候，我们才能说这两棵树是镜像的
+     * 调用递归函数，我们想知道它的左右孩子是否镜像，传入的值是root的左孩子和右孩子。这之前记得判个root==null。
+     */
     public boolean isSymmetricDfs(TreeNode root) {
         if (root == null) return true;
         return isSymmetricDfs(root.left, root.right);
@@ -1255,6 +1275,42 @@ public class SwordFingerOffer {
         //return deque.length == j;
     }
 
+
+    /**
+     * 剑指 Offer 32 - I. 从上到下打印二叉树
+     * https://leetcode.cn/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/
+     *
+     * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+     *
+     * 例如:
+     * 给定二叉树:[3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     *    
+     * 返回：
+     * [3,9,20,15,7]
+     */
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) return new int[0];
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            TreeNode poll = deque.poll();
+            res.add(poll.val);
+            if (poll.left != null) {
+                deque.add(poll.left);
+            }
+            if (poll.right != null) {
+                deque.add(poll.right);
+            }
+        }
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
 
 
     public static void main(String[] args) {
