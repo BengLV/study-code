953c088b06bf6d993fd3556ea9c0f7900465e987
penglu <lt950330>
2022-08-27 16:00:45 +0800

    剑指 Offer 44. 数字序列中某一位的数字

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -1881,6 +1881,46 @@ public class SwordFingerOffer {
         return res;
     }
 
+
+    /**
+     * 剑指 Offer 44. 数字序列中某一位的数字
+     * https://leetcode.cn/problems/shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof/
+     *
+     * 数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从下标0开始计数）是5，第13位是1，第19位是4，等等。
+     *
+     * 请写一个函数，求任意第n位对应的数字。
+     *
+     * 示例 1：
+     * 输入：n = 11
+     * 输出：0
+     *
+     * 数字范围    数量  位数    占多少位
+     *     1-9        9      1       9
+     *     10-99      90     2       180
+     *     100-999    900    3       2700
+     *     1000-9999  9000   4       36000  ...
+     *
+     *     例如 2901 = 9 + 180 + 2700 + 12 即一定是4位数,第12位   n = 12;
+     *     数据为 = 1000 + (12 - 1)/ 4  = 1000 + 2 = 1002
+     *     定位1002中的位置 = (n - 1) %  4 = 3    s.charAt(3) = 2
+     */
+    public int findNthDigit(int n) {
+        int digit = 1;//数字位数
+        long start = 1;//数字范围起始值
+        long count = 9;//各 digit下的数位数量 count
+        //1. 确定所求数位的所在数字的位数,循环执行 nn 减去 一位数、两位数、... 的数位数量 count ，直至 n <= count 时跳出
+        while (n > count) {
+            n -= count;
+            digit += 1;
+            start *= 10;
+            count = digit * start * 9;
+        }
+        //2. 确定所求数位所在的数字
+        long num = start + (n - 1) / digit;
+        //3. 确定所求数位在 num 的哪一数位
+        return Long.toString(num).charAt((n - 1) % digit) - '0';
+    }
+
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(a[0].length);//3
