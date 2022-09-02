4e02dcb8b39a898bb94ff0001d9a63a2f7e2bf08
penglu <lt950330>
2022-09-02 20:14:02 +0800

    剑指 Offer 50. 第一个只出现一次的字符

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2107,6 +2107,28 @@ public class SwordFingerOffer {
         return dp[n - 1];
     }
 
+
+    /**
+     * 剑指 Offer 50. 第一个只出现一次的字符
+     * https://leetcode.cn/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
+     *
+     * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+     *
+     * 示例 1:
+     * 输入：s = "abaccdeff"
+     * 输出：'b'
+     */
+    public char firstUniqChar(String s) {
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if ( arr[s.charAt(i) - 'a'] == 1) return s.charAt(i);
+        }
+        return ' ';
+    }
+
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(a[0].length);//3
