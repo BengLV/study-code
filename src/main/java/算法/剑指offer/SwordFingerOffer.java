d7c7853903ac7731cd45fe392c6f0f88813b1c20
penglu <lt950330>
2022-09-12 13:59:10 +0800

    剑指 Offer 57. 和为s的两个数字

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2452,6 +2452,31 @@ public class SwordFingerOffer {
     }
 
 
+    /**
+     * 剑指 Offer 57. 和为s的两个数字
+     * https://leetcode.cn/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+     *
+     * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+     *
+     *
+     * 示例 1：
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[2,7] 或者 [7,2]
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if ((nums[left] + nums[right]) > target) {
+                right--;
+            } else if ((nums[left] + nums[right]) < target) {
+                left++;
+            } else {
+                return new int[]{nums[left], nums[right]};
+            }
+        }
+        return null;
+    }
+
 
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
@@ -2463,13 +2488,13 @@ public class SwordFingerOffer {
         System.out.println(minArray(new int[]{2, 2, 2, 0, 1}));
         System.out.println(Integer.MAX_VALUE);//2147483647
         System.out.println(printNumbers(1));
-        System.out.println(Arrays.toString(printNumbers1(4).toArray()));
+        //System.out.println(Arrays.toString(printNumbers1(4).toArray()));
         System.out.println(5 / 2);//2
         System.out.println(5 / 2.0);//2.5
         System.out.println(31%100);//31
         System.out.println("123".length());//3
         System.out.println("123".charAt(0));//1
-        System.out.println("123".substring(0, 2));//12
+        System.out.println("subString()" + "012345678".substring(8, 9));//8
         int b[] = new int[5];
         //System.out.println(b[5]);//报错,只能b[4]
         System.out.println("123".toCharArray());
