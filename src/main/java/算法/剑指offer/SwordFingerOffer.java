a977ce6953b07ff4f2d02e170af95ea8b28f1a92
penglu <lt950330>
2022-09-12 14:00:07 +0800

    剑指 Offer 56 - II. 数组中数字出现的次数 II

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2453,6 +2453,26 @@ public class SwordFingerOffer {
 
 
     /**
+     * 剑指 Offer 56 - II. 数组中数字出现的次数 II
+     * https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/
+     *
+     * 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+     *
+     * 示例 1：
+     * 输入：nums = [3,4,3,3]
+     * 输出：4
+     */
+    public int singleNumber(int[] nums) {
+        int a = 0, b = 0;
+        for (int num : nums) {
+            a = (a ^ num) & ~b;
+            b = (b ^ num) & ~a;
+        }
+        return a;
+    }
+
+
+    /**
      * 剑指 Offer 57. 和为s的两个数字
      * https://leetcode.cn/problems/he-wei-sde-liang-ge-shu-zi-lcof/
      *
