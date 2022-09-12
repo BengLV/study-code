029db3852c065eb7316f32076b93e4916b71eadf
penglu <lt950330>
2022-09-12 14:03:07 +0800

    剑指 Offer 58 - I. 翻转单词顺序

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2532,6 +2532,29 @@ public class SwordFingerOffer {
     }
 
 
+    /**
+     * 剑指 Offer 58 - I. 翻转单词顺序
+     * https://leetcode.cn/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+     *
+     * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。
+     * 例如输入字符串"I am a student. "，则输出"student. a am I"。
+     *
+     * 示例 1：
+     * 输入: " hello world! "
+     * 输出:"world! hello"
+     * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     *
+     */
+    public String reverseWords(String s) {
+        String[] ss = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = ss.length - 1; i >= 0; i--) {
+            if (ss[i].equals("")) continue;
+            sb.append(" ").append(ss[i]);
+        }
+        return sb.toString().trim();
+    }
+
 
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
