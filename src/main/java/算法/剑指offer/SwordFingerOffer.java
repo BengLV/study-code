020998a4b6e80374f4f334b3cf6b5f914a2142b1
penglu <lt950330>
2022-09-13 21:34:35 +0800

    剑指 Offer 61. 扑克牌中的顺子

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2668,6 +2668,40 @@ public class SwordFingerOffer {
     }
 
 
+    /**
+     * 剑指 Offer 61. 扑克牌中的顺子
+     * https://leetcode.cn/problems/bu-ke-pai-zhong-de-shun-zi-lcof/
+     *
+     * 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+     * 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+     *
+     * 示例 1:
+     * 输入: [1,2,3,4,5]
+     * 输出: True
+     *
+     * 示例 2:
+     * 输入: [0,0,1,2,5]
+     * 输出: True
+     */
+    public boolean isStraight(int[] nums) {
+        Arrays.sort(nums);//时间复杂度: O(NlogN)
+        int zeroCount = 0, diff = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            } else {
+                if (nums[i + 1] == nums[i]) {
+                    return false;
+                }
+                if (nums[i + 1] - 1 != nums[i]) {
+                    diff += nums[i + 1] - nums[i] - 1;
+                }
+            }
+        }
+        return zeroCount >= diff;
+    }
+
+
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(a[0].length);//3
