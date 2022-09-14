0c68d674b50ea73df11862abda35ab482a0ae6ee
penglu <lt950330>
2022-09-14 19:10:19 +0800

    剑指 Offer 62. 圆圈中最后剩下的数字

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2702,6 +2702,51 @@ public class SwordFingerOffer {
     }
 
 
+    /**
+     * 剑指 Offer 62. 圆圈中最后剩下的数字
+     * https://leetcode.cn/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/?favorite=xb9nqhhg
+     *
+     * 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩下的最后一个数字。
+     *
+     * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+     *
+     * 示例 1：
+     * 输入: n = 5, m = 3
+     * 输出:3
+     *
+     * 示例 2：
+     * 输入: n = 10, m = 17
+     * 输出:2
+     */
+    public int lastRemaining(int n, int m) {
+        /*ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while (list.size() > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);*/
+        /**
+         * f(n) = ( f(n - 1) + m ) % n
+         * 解释意思：
+         *   f(n) 表示上一次
+         *   f(n - 1) 表示这次，因为我们要从这次回推上一次
+         *   m 表示隔几个
+         *   n表示上一次的数组长度
+         */
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = (res + m) % i;
+        }
+        return res;
+    }
+
+
+
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(a[0].length);//3
