e91182a4fb64b3f48ebc0c498894ca28ca38dee7
penglu <lt950330>
2022-09-15 20:42:29 +0800

    剑指 Offer 67. 把字符串转换成整数

--- src/main/java/算法/剑指offer/SwordFingerOffer.java
+++ src/main/java/算法/剑指offer/SwordFingerOffer.java
@@ -2861,6 +2861,54 @@ public class SwordFingerOffer {
         return res;
     }
 
+
+    /**
+     * 剑指 Offer 67. 把字符串转换成整数
+     * https://leetcode.cn/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof/
+     *
+     * 写一个函数 StrToInt，实现把字符串转换成整数这个功能。不能使用 atoi 或者其他类似的库函数。
+     *
+     * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+     * 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；
+     * 假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+     * 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+     * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+     * 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+     *
+     * 说明：
+     * 假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为[−231, 231− 1]。
+     * 如果数值超过这个范围，请返回 INT_MAX (231− 1) 或INT_MIN (−231) 。
+     *
+     */
+    public int strToInt(String str) {
+        //去除前后空格
+        char[] chars = str.trim().toCharArray();
+        if (chars.length == 0) return 0;
+        //结果
+        int res = 0;
+        //符号
+        int sign = 1;
+        //开始遍历的位置
+        int i = 1;
+        //第一个元素为负数
+        if (chars[0] == '-') {
+            sign = -1;
+        } else if (chars[0] != '+') {
+            i = 0;
+        }
+        //用来存储除以10后的值，从而确定是否越界
+        int member = Integer.MAX_VALUE / 10;
+        for (int j = i; j < chars.length; j++) {
+            if (chars[j] > '9' || chars[j] < '0') break;
+            //如果res 大于member，那乘以10肯定越界了  或 如果res 等于 member，后一位如果大于7，那也越界
+            if (res > member || (member == res && chars[j] > '7')) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            res = res * 10 + (chars[j] - '0');
+        }
+        return res * sign;
+    }
+
     public static void main(String[] args) {
         int a[][] = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(a[0].length);//3
