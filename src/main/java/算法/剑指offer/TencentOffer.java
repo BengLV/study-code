eed0b3ecba0a31443832b198bc48c672d44d96a4
penglu <lt950330>
2022-09-19 18:57:38 +0800

    4. 寻找两个正序数组的中位数

--- src/main/java/算法/剑指offer/TencentOffer.java
+++ src/main/java/算法/剑指offer/TencentOffer.java
@@ -63,4 +63,65 @@ public class TencentOffer {
         return res.next;
     }
 
+
+    /**
+     * 4. 寻找两个正序数组的中位数
+     * https://leetcode.cn/problems/median-of-two-sorted-arrays/?favorite=ex0k24j
+     *
+     * 给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
+     *
+     * 算法的时间复杂度应该为 O(log (m+n)) 。
+     *
+     * 示例 1：
+     * 输入：nums1 = [1,3], nums2 = [2]
+     * 输出：2.00000
+     * 解释：合并数组 = [1,2,3] ，中位数 2
+     * 
+     * 示例 2：
+     * 输入：nums1 = [1,2], nums2 = [3,4]
+     * 输出：2.50000
+     * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+     * 
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length = nums1.length + nums2.length;
+        if (length % 2 == 1) {
+            return findMedianSortedArraysHelper(nums1, nums2, length / 2 + 1);
+        } else {
+            return (findMedianSortedArraysHelper(nums1, nums2, length / 2)
+                    + findMedianSortedArraysHelper(nums1, nums2, length / 2 + 1)) / 2;
+        }
+    }
+
+    private double findMedianSortedArraysHelper(int[] nums1, int[] nums2, int k) {
+        int l1 = nums1.length, l2 = nums2.length;
+        int idx1 = 0, idx2 = 0;
+        while (true) {
+            //说明此时nums1数组已经需要全部剔除, 取nums2中第k小的元素
+            if (idx1 == l1) {
+                return nums2[idx2 + k - 1];
+            }
+            if (idx2 == l2) {
+                return nums1[idx1 + k - 1];
+            }
+
+            if (k == 1) {
+                return Math.min(nums1[idx1], nums2[idx2]);
+            }
+            //二分法
+            int half = k / 2;
+            //获取一个新的下标,取两者的最小值,防止越界,同时为了后续判断是否存在于该数组中
+            int newIdx1 = Math.min(idx1 + half, l1) - 1;
+            int newIdx2 = Math.min(idx2 + half, l2) - 1;
+            //如果一个数组的经过二分后的值大于另一个数组, 则剔除最小的那一部分,将小的数组下标更新为新的
+            if (nums1[newIdx1] <= nums2[newIdx2]) {
+                k -= newIdx1 - idx1 + 1;
+                idx1 = newIdx1 + 1;
+            } else {
+                k -= newIdx2 - idx2 + 1;
+                idx2 = newIdx2 + 1;
+            }
+        }
+    }
+
 }
