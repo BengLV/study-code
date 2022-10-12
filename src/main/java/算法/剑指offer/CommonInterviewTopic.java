866d06bbb5faa9702e2fce5796c4dfeab35a5a6a
penglu <lt950330>
2022-10-12 19:51:41 +0800

    912. 排序数组

--- src/main/java/算法/剑指offer/CommonInterviewTopic.java
+++ src/main/java/算法/剑指offer/CommonInterviewTopic.java
@@ -1,5 +1,6 @@
 package 算法.剑指offer;
 
+
 /**
  * @description: 牛客网高频面试题
  * https://www.nowcoder.com/exam/oj?page=1&tab=%E7%AE%97%E6%B3%95%E7%AF%87&topicId=117
@@ -30,9 +31,38 @@ public class CommonInterviewTopic {
     }
 
 
+    /**
+     * 912. 排序数组
+     * 给你一个整数数组 nums，请你将该数组升序排列。
+     *
+     * 示例 1：
+     * 输入：nums = [5,2,3,1]
+     * 输出：[1,2,3,5]
+     */
     public int[] sortArray(int[] nums) {
-        return null;
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
     }
+
+    private void quickSort(int[] arr, int l, int r) {
+        if (r <= l) return;
+        int i = l, j = r;
+        while (i < j) {
+            while (i < j && arr[j] >= arr[l]) j--;
+            while (i < j && arr[i] <= arr[l]) i++;
+            swap(arr, i, j);
+        }
+        swap(arr, l, i);
+        quickSort(arr, l, i - 1);
+        quickSort(arr, i + 1, r);
+    }
+
+    private void swap(int[] arr, int l, int r) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+
 
 }
 
