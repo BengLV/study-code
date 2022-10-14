0b30a9865d51562fe06f82783dba2d009b680b61
penglu <lt950330>
2022-10-14 19:10:07 +0800

    146. LRU 缓存

--- src/main/java/算法/剑指offer/CommonInterviewTopic.java
+++ src/main/java/算法/剑指offer/CommonInterviewTopic.java
@@ -1,6 +1,9 @@
 package 算法.剑指offer;
 
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @description: 牛客网高频面试题
  * https://www.nowcoder.com/exam/oj?page=1&tab=%E7%AE%97%E6%B3%95%E7%AF%87&topicId=117
@@ -62,6 +65,69 @@ public class CommonInterviewTopic {
         arr[l] = arr[r];
         arr[r] = temp;
     }
+
+    /**
+     * 146. LRU 缓存
+     * https://leetcode.cn/problems/lru-cache/
+     *
+     * 请你设计并实现一个满足 LRU (最近最少使用) 缓存 约束的数据结构。
+     * 实现 LRUCache 类：
+     * LRUCache(int capacity) 以 正整数 作为容量capacity 初始化 LRU 缓存
+     * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+     * void put(int key, int value)如果关键字key 已经存在，则变更其数据值value ；如果不存在，则向缓存中插入该组key-value 。
+     * 如果插入操作导致关键字数量超过capacity ，则应该 逐出 最久未使用的关键字。
+     * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+     *
+     */
+    class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+        private Integer capacity;
+
+        public LRUCache(int capacity) {
+            //容量, 负载银子, true:按照读取顺序排序, false:按照插入顺序排序
+            super(capacity, 0.75F, true);
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            return super.getOrDefault(key, -1);
+        }
+
+        public void put(int key, int value) {
+            super.put(key, value);
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return size() > capacity;
+        }
+    }
+
+
+   /* public class Solution {
+
+        LinkedHashMap<Integer, Integer> map = null;
+        int capacity = 0;
+
+        public Solution(int capacity) {
+            // write code here
+            map = new LinkedHashMap<>(capacity, 0.75F, true);
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            // write code here
+            return map.getOrDefault(key, -1);
+        }
+
+        public void set(int key, int value) {
+            // write code here
+            if (map.size() == capacity) {
+                map.remove(map.keySet().iterator().next());
+            }
+            map.put(key, value);
+        }
+    }*/
 
 
 }
