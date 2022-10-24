1f77a7476314b2c877c4888819db36f6c5de49fd
penglu <lt950330>
2022-10-24 20:26:30 +0800

    NC61 两数之和

--- src/main/java/算法/剑指offer/CommonInterviewTopic.java
+++ src/main/java/算法/剑指offer/CommonInterviewTopic.java
@@ -248,6 +248,29 @@ public class CommonInterviewTopic {
         return a[n - K];
     }
 
+    /**
+     * NC61 两数之和
+     *
+     * 给出一个整型数组 numbers 和一个目标值 target，请在数组中找出两个加起来等于目标值的数的下标，返回的下标按升序排列。
+     * （注：返回的数组下标从1开始算起，保证target一定可以由数组里面2个数字相加得到）
+     */
+    public int[] twoSum (int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>(numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                if (i > map.get(target - numbers[i])) {
+                    return new int[]{map.get(target - numbers[i]) + 1, i + 1};
+                } else {
+                    return new int[]{i + 1, map.get(target - numbers[i]) + 1};
+                }
+            } else {
+                map.put(numbers[i], i);
+            }
+        }
+        return null;
+    }
+
+
 }
 
 
