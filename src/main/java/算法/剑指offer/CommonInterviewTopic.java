76e59dc907ad21759a05508601ecb7520d4f901c
penglu <lt950330>
2022-10-25 19:42:33 +0800

    NC76 用两个栈实现队列

--- src/main/java/算法/剑指offer/CommonInterviewTopic.java
+++ src/main/java/算法/剑指offer/CommonInterviewTopic.java
@@ -276,7 +276,7 @@ public class CommonInterviewTopic {
      * 输入两个递增的链表，单个链表的长度为n，合并这两个链表并使新链表中的节点仍然是递增排序的。
      *
      */
-    public ListNode Merge(ListNode list1,ListNode list2) {
+    public ListNode Merge(ListNode list1, ListNode list2) {
         if (list1 == null) {
             return list2;
         }
@@ -292,6 +292,31 @@ public class CommonInterviewTopic {
         }
     }
 
+    /**
+     * NC76 用两个栈实现队列
+     *
+     * 用两个栈来实现一个队列，使用n个元素来完成 n 次在队列尾部插入整数(push)和n次在队列头部删除整数(pop)的功能。 队列中的元素为int类型。保证操作合法，即保证pop操作时队列内已有元素。
+     *
+     * 要求：存储n个元素的空间复杂度为 O(n)，插入与删除的时间复杂度都是 O(1)
+     */
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            if (stack1.isEmpty()) {
+                return -1;
+            }
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
+        return stack2.pop();
+    }
 
 }
 
