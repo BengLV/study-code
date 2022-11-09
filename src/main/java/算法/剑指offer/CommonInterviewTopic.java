ad95c46072398c2097bdeb25275bd900dece4a5e
penglu <lt950330>
2022-11-09 21:19:08 +0800

    NC52 有效括号序列

--- src/main/java/算法/剑指offer/CommonInterviewTopic.java
+++ src/main/java/算法/剑指offer/CommonInterviewTopic.java
@@ -512,6 +512,22 @@ public class CommonInterviewTopic {
         return null;
     }
 
+    /**
+     * NC52 有效括号序列
+     * 给出一个仅包含字符'(',')','{','}','['和']',的字符串，判断给出的字符串是否是合法的括号序列
+     * 括号必须以正确的顺序关闭，"()"和"()[]{}"都是合法的括号序列，但"(]"和"([)]"不合法
+     */
+    public boolean isValid(String s) {
+        Stack<Character>stack = new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(c=='(')stack.push(')');
+            else if(c=='[')stack.push(']');
+            else if(c=='{')stack.push('}');
+            else if(stack.isEmpty()||c!=stack.pop())return false;
+        }
+        return stack.isEmpty();
+    }
+
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
