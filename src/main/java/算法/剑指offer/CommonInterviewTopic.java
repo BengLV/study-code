e8626dceeff06db31cf3f23e88a48b16fe110ad1
penglu <lt950330>
2022-12-07 20:00:27 +0800

    NC48 在旋转过的有序数组中寻找目标值 33. 搜索旋转排序数组

--- src/main/java/算法/剑指offer/CommonInterviewTopic.java
+++ src/main/java/算法/剑指offer/CommonInterviewTopic.java
@@ -941,6 +941,45 @@ public class CommonInterviewTopic {
     }
 
 
+    /**
+     * NC48 在旋转过的有序数组中寻找目标值
+     * 33. 搜索旋转排序数组
+     * https://leetcode.cn/problems/search-in-rotated-sorted-array/
+     *
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+     *
+     * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。(二分法)
+     */
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[right]) {
+                //如果中间的值小于最右边的值，说明右半段是有序的。
+                if(nums[mid] < target && target <= nums[right]) {
+                    //如果中间的值小于目标值并且目标值小于最右边的值(防止目标值过大不存在)
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                //说明左半段是有序的。
+                if(target < nums[mid] && nums[left] <= target) {
+                    //如果最左边的值小于目标值
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
 
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
