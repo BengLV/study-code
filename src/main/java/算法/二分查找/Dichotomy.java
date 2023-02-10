ed81909b97a8650d294d83db1a0a4aeb1cd2fe4b
penglu <lt950330>
2023-02-10 21:13:54 +0800

    162. 寻找峰值 BM19 寻找峰值

--- src/main/java/算法/二分查找/Dichotomy.java
+++ src/main/java/算法/二分查找/Dichotomy.java
@@ -68,4 +68,31 @@ public class Dichotomy {
         return false;
     }
 
+    /**
+     * 162. 寻找峰值
+     * BM19 寻找峰值
+     * https://leetcode.cn/problems/find-peak-element/
+     *
+     * 峰值元素是指其值严格大于左右相邻值的元素。
+     * 给你一个整数数组nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+     * 你可以假设nums[-1] = nums[n] = -∞ 。
+     * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。 (时间复杂度限制只能用二分查找)
+     * 对于所有有效的 i 都有 nums[i] != nums[i + 1]
+     *
+     * 爬坡法(想象出两个单调递增或递减的连续函数)
+     */
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            //如果右边的元素大于左边的,则在右边一定有峰值
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;//返回right也行
+    }
+
 }
