6dc02a0654d4c7099a88697525475efa01b1d7bb
penglu <lt950330>
2023-03-02 19:45:14 +0800

    746. 使用最小花费爬楼梯 BM64 最小花费爬楼梯

--- src/main/java/算法/dynamicprogram/DynamicProgramming.java
+++ src/main/java/算法/dynamicprogram/DynamicProgramming.java
@@ -77,6 +77,7 @@ public class DynamicProgramming {
 
     /**
      * 746. 使用最小花费爬楼梯
+     * BM64 最小花费爬楼梯
      * https://leetcode.cn/problems/min-cost-climbing-stairs/
      * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
      * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
@@ -103,6 +104,11 @@ public class DynamicProgramming {
      *
      */
     public int minCostClimbingStairs(int[] cost) {
+        /*int[] dp = new int[cost.length + 1];
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.length];*/
         int[] dp = new int[cost.length + 1];
         dp[1] = cost[0];
         dp[2] = cost[1];
