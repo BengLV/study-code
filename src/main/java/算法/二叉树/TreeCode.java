2b5dc2c89dd60bc5fb956fb6923c789c3e59f040
penglu <lt950330>
2023-03-06 21:27:41 +0800

    剑指 Offer 27. 二叉树的镜像 BM33 二叉树的镜像

--- src/main/java/算法/二叉树/TreeCode.java
+++ src/main/java/算法/二叉树/TreeCode.java
@@ -801,6 +801,36 @@ public class TreeCode {
         treeToDoublyListDfs(node.right);
     }
 
+    /**
+     * 剑指 Offer 27. 二叉树的镜像
+     * BM33 二叉树的镜像
+     * https://leetcode.cn/problems/er-cha-shu-de-jing-xiang-lcof/
+     */
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop(), temp;
+            temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+        }
+
+        /*if (root == null) return null;
+        TreeNode node = new TreeNode(root.val);
+        node.left = mirrorTree(root.right);
+        node.right = mirrorTree(root.left);
+        return node;*/
+        return root;
+    }
+
 
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5, 0};
