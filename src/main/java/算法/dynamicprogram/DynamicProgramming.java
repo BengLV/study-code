a25b8b5f509fbc42c762ee8ea19108906ee727c8
penglu <lt950330>
2023-03-23 20:32:08 +0800

    198. 打家劫舍 BM78 打家劫舍(一)

--- src/main/java/算法/dynamicprogram/DynamicProgramming.java
+++ src/main/java/算法/dynamicprogram/DynamicProgramming.java
@@ -384,6 +384,35 @@ public class DynamicProgramming {
         return dp[amount] == max ? -1 : dp[amount];
     }
 
+
+    /**
+     * 198. 打家劫舍
+     * BM78 打家劫舍(一)
+     *
+     * 你是一个经验丰富的小偷，准备偷沿街的一排房间，每个房间都存有一定的现金，为了防止被发现，你不能偷相邻的两家，
+     * 即，如果偷了第一家，就不能再偷第二家；如果偷了第二家，那么就不能偷第一家和第三家。
+     * 给定一个整数数组nums，数组中的元素表示每个房间存有的现金数额，请你计算在不被发现的前提下最多的偷窃金额。
+     */
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i-1]);
+        }
+        return dp[length - 1];
+    }
+
     public static void main(String[] args) {
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
         System.out.println(objectObjectHashMap.get("21"));
