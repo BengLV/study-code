afee028d1009a8a78873142072acf838132b369d
penglu <lt950330>
2023-08-01 20:45:59 +0800

    69. x 的平方根

--- src/main/java/算法/二分查找/Dichotomy.java
+++ src/main/java/算法/二分查找/Dichotomy.java
@@ -132,4 +132,29 @@ public class Dichotomy {
         }
     }
 
+
+    /**
+     * 69. x 的平方根
+     * https://leetcode.cn/problems/sqrtx/description/
+     */
+    public int mySqrt(int x) {
+        if (x == 1) {
+            return x;
+        }
+        int left = 1;
+        int right = x;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int temp = x / mid;
+            if (temp == mid) {
+                return mid;
+            } else if (temp < mid) {
+                right =  mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
 }
