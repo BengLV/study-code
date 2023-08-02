52ed435d0cdca4a12fddd591bd9277645673a843
penglu <lt950330>
2023-08-02 21:04:54 +0800

    8. 字符串转换整数 (atoi)

--- src/main/java/算法/字符串/StringCode.java
+++ src/main/java/算法/字符串/StringCode.java
@@ -38,4 +38,54 @@ public class StringCode {
     }
 
 
+    /**
+     * 8. 字符串转换整数 (atoi)
+     * https://leetcode.cn/problems/string-to-integer-atoi/description/
+     *
+     * 解题思路:
+     * 1. 首先判断前面是否有空格
+     * 2. 判断正负标识
+     * 3. 数字计算方法, 字符转换成数字的方法,以及判断是否是否为数字
+     * 4. 判断数字是否超出范围的技巧,以及需要注意 214748364 + n
+     *    如果 n 大于 7 时, 会自动将数组转换成负数.
+     */
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int idx = 0, len = s.length();
+        //前导空格
+        while (idx < len && s.charAt(idx) == ' ') {
+            idx++;
+        }
+        //判断符合
+        if (idx == len) {
+            return 0;
+        }
+
+        int flag = 1;
+        if (s.charAt(idx) == '-' || s.charAt(idx) == '+') {
+            flag = s.charAt(idx) == '-' ? -1 : 1;
+            idx++;
+        }
+
+        if (idx == len) {
+            return 0;
+        }
+        int res = 0;
+        while (idx < len && s.charAt(idx) >= '0' && s.charAt(idx) <= '9') {
+            int num = s.charAt(idx) - '0';
+            //处理超出范围
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && num > 7)) {
+                return flag == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            res = res * 10 + num;
+            idx++;
+        }
+        return res * flag;
+
+
+    }
+
+
 }
